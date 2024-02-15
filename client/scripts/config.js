55679b90a79f655c83833aaa12ff8e1e872b9172
Spax04 <gotlib14@gmail.com>
2024-02-15 10:35:42 +0200

    setting page done

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -1,44 +1,86 @@
-document.getElementById("lockBtn").addEventListener("click", function () {
-    var unlockForm = document.getElementById("unlockForm");
-    var blockLevelSelect = document.getElementById("blockLevel");
-    var logoutBtn = document.getElementById('logout-btn');
+document.getElementById('lockBtn').addEventListener('click', function () {
+  var unlockForm = document.getElementById('unlockForm')
+  var blockLevelSelect = document.getElementById('blockLevel')
+  var logoutBtn = document.getElementById('logout-btn')
 
-    if (unlockForm.style.display === "none") {
-        unlockForm.style.display = "block";
-        blockLevelSelect.disabled = true;
-        this.style.display = "none";
-        logoutBtn.style.display = "none"
-    } else {
-        unlockForm.style.display = "none";
-        blockLevelSelect.disabled = false;
-        this.value = "Lock";
-    }
-});
+  if (unlockForm.style.display === 'none') {
+    unlockForm.style.display = 'block'
+    blockLevelSelect.disabled = true
+    this.style.display = 'none'
+    logoutBtn.style.display = 'none'
+  } else {
+    unlockForm.style.display = 'none'
+    blockLevelSelect.disabled = false
+    this.value = 'Lock'
+  }
+})
 
-document.getElementById("unlockBtn").addEventListener("click", function () {
-    var unlockPassword = document.getElementById("unlockPassword").value;
+document.getElementById('unlockBtn').addEventListener('click', function () {
+  var unlockPassword = document.getElementById('unlockPassword').value
+  let token
+  chrome.storage.local.get(['token']).then(result => {
+    token = result
+  })
+  // fetch("https://your-api-endpoint.com/login", {
+  //     method: "POST",
+  //     headers: {
+  //         "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({  password: unlockPassword,token:token })
+  // })
+  // .then(response => {
+  //     if (!response.ok) {
+  //         alert("Login failed. Please check your credentials and try again.");
+  //     }
+  //     return response.json();
+  // })
+  // .then(data => {
+  //     if(data.isCorrect){
 
-    var isPasswordCorrect = true;
+  //         var unlockForm = document.getElementById("unlockForm");
+  //         var blockLevelSelect = document.getElementById("blockLevel");
 
-    if (isPasswordCorrect) {
-        var unlockForm = document.getElementById("unlockForm");
-        var blockLevelSelect = document.getElementById("blockLevel");
+  //         unlockForm.style.display = "none";
+  //         blockLevelSelect.disabled = false;
 
-        unlockForm.style.display = "none";
-        blockLevelSelect.disabled = false;
+  //         // Optionally, reset the password input field
+  //         document.getElementById("unlockPassword").value = "";
 
-        // Optionally, reset the password input field
-        document.getElementById("unlockPassword").value = "";
+  //         // Change the Lock button back to its original state
+  //         document.getElementById("lockBtn").style.display = "block";
+  //         document.getElementById('logout-btn').style.display = "block";
+  //     }else{
+  //         alert("Incorrect password. Please try again.");
+  //     }
+  // })
+  // .catch(error => {
+  //     console.log(error);
+  // });
 
-        // Change the Lock button back to its original state
-        document.getElementById("lockBtn").style.display = "block";
-        document.getElementById('logout-btn').style.display = "block";
-    } else {
-        alert("Incorrect password. Please try again.");
-    }
-});
+  //! TEST SECTION
 
-function logout() {
-    localStorage.removeItem("token");
-    window.location.href = "login.html";
+  var unlockForm = document.getElementById('unlockForm')
+  var blockLevelSelect = document.getElementById('blockLevel')
+
+  unlockForm.style.display = 'none'
+  blockLevelSelect.disabled = false
+
+  // Optionally, reset the password input field
+  document.getElementById('unlockPassword').value = ''
+
+  // Change the Lock button back to its original state
+  document.getElementById('lockBtn').style.display = 'block'
+  document.getElementById('logout-btn').style.display = 'block'
+})
+
+document.getElementById("whiteList").addEventListener('click',redirectToWhiteLIst)
+function redirectToWhiteLIst(){
+    console.log("Button clicked"); // Check if this message appears in the console
+    chrome.tabs.create({ url: chrome.runtime.getURL('../pages/settings.html') });
+};
+
+document.getElementById('logout-btn').addEventListener('click', logout);
+function logout () {
+  localStorage.removeItem('token')
+  window.location.href = '../pages/login.html'
 }
--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,47 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Check if a token already exists in localStorage
-    let token;
-    chrome.storage.local.get(['token']).then(result => {
-        token = result
-      })
-    if (token) {
-        // Redirect to the configuration page
-        window.location.href = "../pages/config.html";
-    }
-});
+document.addEventListener('DOMContentLoaded', function () {
+  // Check if a token already exists in localStorage
+  let token
+  chrome.storage.local.get(['token']).then(result => {
+    token = result
+  })
+  if (token) {
+    // Redirect to the configuration page
+    window.location.href = '../pages/config.html'
+  }
+})
 
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission
+document
+  .getElementById('loginForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault() // Prevent the default form submission
 
     // Get the username and password from the form
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
 
     // Make a fetch request to your API endpoint
-    fetch("https://your-api-endpoint.com/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ username: username, password: password })
-    })
-    .then(response => {
-        if (!response.ok) {
-            alert("Login failed. Please check your credentials and try again.");
-        }
-        return response.json();
-    })
-    .then(data => {
-        let token = data.token;
-        let userId = data.userId
-        chrome.storage.local.set({ "token": token });
-        chrome.storage.local.set({ "userId": userId });
+    // fetch("https://your-api-endpoint.com/login", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({ username: username, password: password })
+    // })
+    // .then(response => {
+    //     if (!response.ok) {
+    //         alert("Login failed. Please check your credentials and try again.");
+    //     }
+    //     return response.json();
+    // })
+    // .then(data => {
+    //     let token = data.token;
+    //     let userId = data.userId
+    //     chrome.storage.local.set({ "token": token });
+    //     chrome.storage.local.set({ "userId": userId });
 
-        window.location.href = "../pages/config.html";
-    })
-    .catch(error => {
-        window.location.href = "../pages/config.html";
-        console.error("Error:", error);
-        alert(error.message);
-    });
-});
+    //     window.location.href = "../pages/config.html";
+    // })
+    // .catch(error => {
+    //     window.location.href = "../pages/config.html";
+    //     console.error("Error:", error);
+    //     alert(error.message);
+    // });
+
+    window.location.href = '../pages/config.html'
+  })
